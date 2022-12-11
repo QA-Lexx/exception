afc099f4ca9153ea73cbd39548957aa59884888b
QA-Lexx <ilin_001@mail.ru>
2022-12-11 21:22:41 +0300

    Create project exception

---
+++ src/main/java/ProductRepository.java
@@ -0,0 +1,43 @@
+public class ProductRepository {
+    private static Product[] products = new Product[0];
+
+    public void addProduct(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product[] removeProductById(int idRemove) {
+        if (findById(idRemove) == null) {
+            throw new NotFoundException(idRemove);
+        }
+
+
+        Product[] tmp = new Product[products.length - 1];
+        int indexInsert = 0;
+        for (Product product : products) {
+            if (product.getId() != idRemove) {
+                tmp[indexInsert] = product;
+                indexInsert++;
+            }
+            products = tmp;
+        }
+        return tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static Product[] findAll() {
+        return products;
+    }
+}
